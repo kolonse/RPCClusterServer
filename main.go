1f85977e455d205bf18f86f5ce08ba3e8f8ab97f
kolonse <KolonseTiger@gmail.com>
2015-11-27 20:13:17 +0800

    完成TCP服务的启动

--- main.go
+++ main.go
@@ -2,25 +2,42 @@
 package main
 
 import (
+	"encoding/json"
+	"github.com/kolonse/TCPServer"
 	"github.com/kolonse/kolonsecfg"
 	"github.com/kolonse/logs"
+	"os"
+	"path"
 )
 
 func main() {
 	cfg := kolonsecfg.NewCfg()
 	cfg.ParseFile(*cfgPath)
+	cfgNode := cfg.Child(*env)
 	logger := logs.NewLogger(10000)
-	//	{
-	//		"filename":"logs/beego.log",
-	//		"maxlines":10000,
-	//		"maxsize":1<<30,
-	//		"daily":true,
-	//		"maxdays":15,
-	//		"rotate":true
-	//	}
-	err := logger.SetLogger("file", "")
+	// 创建配置日志文件目录
+	os.MkdirAll(cfgNode.Child("log.logdir").GetString(), 0777)
+	filename := path.Join(cfgNode.Child("log.logdir").GetString(),
+		cfgNode.Child("log.filename").GetString())
+	logCfg := map[string]interface{}{
+		"filename": filename,
+		"maxlines": cfgNode.Child("log.maxlines").GetInt(),
+		"maxsize":  cfgNode.Child("log.maxsize").GetInt(),
+		"daily":    cfgNode.Child("log.daily").GetBool(),
+		"maxdays":  cfgNode.Child("log.maxdays").GetInt(),
+		"rotate":   cfgNode.Child("log.rotate").GetBool(),
+	}
+	buff, _ := json.Marshal(logCfg)
+	err := logger.SetLogger("file", string(buff))
 	if err != nil {
 		panic(err.Error())
 	}
-	logger.Info("%v", cfg.Dump())
+	err = logger.SetLogger("console", "")
+	if err != nil {
+		panic(err.Error())
+	}
+	logger.Info("当前环境:%v 加载配置:\n%v", *env, cfgNode.Dump(""))
+	ts := TCPServer.NewTCPServer(cfgNode.Child("serveraddr").GetString())
+	ts.Register("logger", logger)
+	ts.Server()
 }
